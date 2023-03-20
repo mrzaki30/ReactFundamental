856093a9063b79c0611d2b5a6e486962b7b14143
zaki <zaki.ilhami46@gmail.com>
2023-03-20 14:02:58 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import { useState } from 'react'
 import './App.css';
+import shoppingIcon from './assets/shopping-icon.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <nav className="nav" >
+                <img className="nav-icon" src={shoppingIcon} alt="Shopping Cart" />
+                <h1 className="nav-title">Shopping Cart</h1>
+            </nav>
+        </>
+    );
 }
 
 export default App;
