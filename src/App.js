d16d7785fa60b4c3d71b4e441d4ba13e3557ecdf
zaki <zaki.ilhami46@gmail.com>
2023-03-22 13:40:37 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-// import logo from './logo.svg';
-// import { useState } from 'react'
 import './App.css';
 import shoppingIcon from './assets/shopping-icon.svg';
+import { useState } from 'react';
 
 function App() {
+    const [value, setValue] = useState('');
+    const [todos, setTodos] = useState([
+        { text: 'Learn React', count: 1 },
+        { text: 'Learn Firebase', count: 1 },
+        { text: 'Learn GraphQL', count: 1 },
+    ]);
+    // console.log(todos);
+    console.log(value);
     return (
         <>
             <nav className="nav" >
                 <img className="nav-icon" src={shoppingIcon} alt="Shopping Cart" />
                 <h1 className="nav-title">Shopping Cart</h1>
             </nav>
+            <section className="container">
+                <form className="form">
+                    <input onChange={(e) => {
+                        setValue(e.target.value)
+                    }}
+                        value={value} type="text" className="input" placeholder="List" />
+                    <button className="add-button" type="submit">add</button>
+                </form>
+                {todos.length > 0 ? (
+                    <div className="todos"> todoss</div>
+                ) : (
+                    <div className="empty">No todos</div>
+                )}
+            </section>
         </>
     );
 }
